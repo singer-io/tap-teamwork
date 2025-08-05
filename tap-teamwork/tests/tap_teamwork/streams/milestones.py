89e20ad4caab43853446ccd13bdc37b44ab1f9d2
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-08-05 15:21:04 +0530

    Fixed: Folder structure for gl-master

---
+++ tap-teamwork/tests/tap_teamwork/streams/milestones.py
@@ -0,0 +1,13 @@
+from typing import Dict, Iterator, List
+from singer import get_logger
+from tap_teamwork.streams.abstracts import FullTableStream
+
+LOGGER = get_logger()
+
+class Milestones(FullTableStream):
+    tap_stream_id = "milestones"
+    key_properties = ["id"]
+    replication_method = "FULL_TABLE"
+    replication_keys: List[str] = []
+    data_key = "milestones"
+    path = "projects/api/v3/milestones.json"
