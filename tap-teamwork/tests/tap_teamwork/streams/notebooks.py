d615be83645fecc4e0f1583d7c8319ba80b1d573
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-08-05 15:21:04 +0530

    Fixed: Folder structure for gl-master

---
+++ tap-teamwork/tests/tap_teamwork/streams/notebooks.py
@@ -0,0 +1,13 @@
+from typing import Dict, Iterator, List
+from singer import get_logger
+from tap_teamwork.streams.abstracts import FullTableStream
+
+LOGGER = get_logger()
+
+class Notebooks(FullTableStream):
+    tap_stream_id = "notebooks"
+    key_properties = ["id"]
+    replication_method = "FULL_TABLE"
+    replication_keys: List[str] = []
+    data_key = "notebooks"
+    path = "projects/api/v3/notebooks.json"
---
+++ tap-teamwork/tests/tap_teamwork/streams/ticket_details.py
@@ -0,0 +1,14 @@
+from typing import Dict, Iterator, List
+from singer import get_logger
+from tap_teamwork.streams.abstracts import FullTableStream
+
+LOGGER = get_logger()
+
+
+class TicketDetails(FullTableStream):
+    tap_stream_id = "ticket_details"
+    key_properties = ["id"]
+    replication_method = "FULL_TABLE"
+    replication_keys: List[str] = []
+    data_key = "ticket"
+    path = "desk/v1/tickets/{ticketId}.json"
