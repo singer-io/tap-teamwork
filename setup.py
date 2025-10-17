782b57cb2f138838be4f946a25c4a890d14f48b2
Rutuja Deshmukh <107538720+rdeshmukh15@users.noreply.github.com>
2025-10-17 18:13:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-teamwork",
-    version="1.0.0",
+    version="0.0.1",
     description="Singer.io tap for extracting data from Teamwork API",
     author="Stitch",
     url="http://singer.io",
