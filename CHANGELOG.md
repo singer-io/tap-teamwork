2be7e990232e2d899517f24bf3f5e45fd4e8dad9
Ranit Saha <ranit.saha@qlik.com>
2026-03-05 15:30:11 +0530

    updated versions and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
 # Changelog
 
 ## 0.1.0
-  * Updated python version, Added integration tests. [#19](https://github.com/singer-io/tap-teamwork/pull/19)
+  * Updated python version. [#19](https://github.com/singer-io/tap-teamwork/pull/19)
+  * Added integration tests.
 
 ## 0.0.1
   * Initial Commit
--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_teamwork"],
     install_requires=[
-        "singer-python==6.1.1",
-        "requests==2.32.4",
+        "singer-python==6.8.0",
+        "requests==2.32.5",
         "backoff==2.2.1"
     ],
     entry_points="""
